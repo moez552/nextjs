9cced82198f78724e3d5cb73bdcb5e6b074bd595
moez552 <moez.zhioua@gmail.com>
2022-06-11 19:43:11 +0100

    xx

--- middleware/protectAPI.js
+++ middleware/protectAPI.js
@@ -3,7 +3,6 @@ const protectAPI = (handler) => {
         if(new URL(req.headers.referer) !== 'https://nextjs-n9lu7ozrj-moez552.vercel.app') {
             return res.status(403).json({success: false, message: `Forbidden`})
         }
-        return ({'passed':'yes'})
         return handler(req, res)
     }
 }
